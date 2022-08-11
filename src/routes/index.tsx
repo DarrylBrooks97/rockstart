8e49dd54d90162f175b6f448fd659d9c8cd02a8d
Darryl Brooks <darryl.brooks@aa.com>
2022-08-11 11:28:12 -0500

    feat: paint job

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,19 @@
-import Counter from "~/components/Counter";
-import "./index.css";
+import Counter from '~/components/Counter';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
+    <main className="bg-black h-screen w-screen p-3">
+      <div className="p-3 flex flex-col justify-center items-center h-full w-full">
+        <h1 className="text-white text-5xl p-3">Testing SolidJS !</h1>
+        <Counter />
+        <p>
+          Visit{' '}
+          <a href="https://solidjs.com" target="_blank">
+            solidjs.com
+          </a>{' '}
+          to learn how to build Solid apps.
+        </p>
+      </div>
     </main>
   );
 }
